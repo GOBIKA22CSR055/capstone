3cd78cd0a0180f9ab03fb32b8a8451362dafd680
Gobika S <gobikas.22cse@kongu.edu>
2025-03-20 17:02:59 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
-    
+    docker login -u gobi9080377604 -p dckr_pat_BVFA2aEatL12F6ctw-EgNZ_sIx8
+    docker tag test1 gobi9080377604/task2
+    docker push gobi9080377604/task2
